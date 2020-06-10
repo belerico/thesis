8ae599b77e628a6d62ce65ab5d67ade5565fa555
belerico <belo.fede@outlook.com>
2020-06-10 00:24:45 +0000

    Add automatic logger with experiments

--- codalab_challenge/train_cade.py
+++ codalab_challenge/train_cade.py
@@ -1,3 +1,4 @@
+import datetime
 import logging
 import os
 
@@ -6,64 +7,113 @@ from gensim.models.word2vec import Word2Vec
 
 from preprocess import create_compass
 
-logging.basicConfig(format="%(levelname)s:%(message)s", level=logging.DEBUG)
+config = {
+    "LANG": ["latin", "english", "swedish", "german"],
+    "EMBEDDING_SIZE": 100,
+    "SITER": 10,
+    "DITER": 10,
+    "PREPROCESS": False,
+}
 
-
-LANG = ["english", "latin", "swedish", "german"]
-EMBEDDING_SIZE = 100
-SITER = 5
-DITER = 5
 
 if __name__ == "__main__":
-    for lang in LANG:
+    if not os.path.exists("./experiments"):
+        os.makedirs("./experiments")
+
+    new_exp_num = 0
+    for exp in os.listdir("./experiments"):
+        exp_num = int(exp.split("_")[1])
+        if exp_num > exp_num:
+            new_exp_num = exp_num
+    CURRENT_EXP_DIR = "./experiments/experiment_" + str(new_exp_num)
+    if not os.path.exists(CURRENT_EXP_DIR):
+        os.makedirs(CURRENT_EXP_DIR)
+
+    logger = logging.getLogger("thesis")
+    fh = logging.FileHandler(
+        CURRENT_EXP_DIR + "/experiment_" + str(new_exp_num) + ".log"
+    )
+    fh.setLevel(logging.DEBUG)
+    fh.setFormatter(
+        logging.Formatter(
+            "%(asctime)s|%(levelname)s %(message)s", "%Y-%m-%d %H:%M:%S"
+        )
+    )
+    logger.addHandler(fh)
+
+    logger.info(
+        "Experiment "
+        + str(new_exp_num)
+        + ": "
+        + datetime.datetime.today().strftime("%Y-%m-%d %H:%M:%S")
+    )
+    logger.info("Current config:")
+    for k, v in config.items():
+        logger.info(k + ": " + str(v))
+
+    for lang in config["LANG"]:
         # Create compass
-        logging.info({"Creating compass for " + lang.upper()})
-        if not os.path.exists("./model/" + lang):
-            os.makedirs("./model/" + lang)
+        logger.info("Creating compass for " + lang.upper())
+        if not os.path.exists(CURRENT_EXP_DIR + "/model/" + lang):
+            os.makedirs(CURRENT_EXP_DIR + "/model/" + lang)
         create_compass(
             [
                 "./data/"
                 + lang
                 + "/semeval2020_ulscd_"
                 + lang[:3]
-                + "/corpus1/lemma/ccoha1.txt",
+                + "/corpus1/lemma/corpus1.txt",
                 "./data/"
                 + lang
                 + "/semeval2020_ulscd_"
                 + lang[:3]
-                + "/corpus2/lemma/ccoha2.txt",
+                + "/corpus2/lemma/corpus2.txt",
             ],
-            save_path="./model/" + lang + "/compass_" + lang[:3] + ".txt",
+            save_path=CURRENT_EXP_DIR
+            + "/model/"
+            + lang
+            + "/compass_"
+            + lang[:3]
+            + ".txt",
         )
 
         aligner = CADE(
-            size=EMBEDDING_SIZE,
-            opath="./model/" + lang,
-            siter=SITER,
-            diter=DITER,
+            size=config["EMBEDDING_SIZE"],
+            opath=CURRENT_EXP_DIR + "/model/" + lang,
+            siter=config["SITER"],
+            diter=config["DITER"],
         )
 
         # train the compass: the text should be the concatenation of the text from the slices
+        logger.info("Training compass model")
         aligner.train_compass(
-            "./model/" + lang + "/compass_" + lang[:3] + ".txt", overwrite=True
+            CURRENT_EXP_DIR
+            + "/model/"
+            + lang
+            + "/compass_"
+            + lang[:3]
+            + ".txt",
+            overwrite=True,
         )
 
         # now you can train slices and they will be already aligned
         # these are gensim word2vec objects
+        logger.info("Training slice1 model")
         slice_one = aligner.train_slice(
             "./data/"
             + lang
             + "/semeval2020_ulscd_"
             + lang[:3]
-            + "/corpus1/lemma/ccoha1.txt",
+            + "/corpus1/lemma/corpus1.txt",
             save=True,
         )
+        logger.info("Training slice2 model")
         slice_two = aligner.train_slice(
             "./data/"
             + lang
             + "/semeval2020_ulscd_"
             + lang[:3]
-            + "/corpus2/lemma/ccoha2.txt",
+            + "/corpus2/lemma/corpus2.txt",
             save=True,
         )
 
